bff98327c4ef4d44b15c93d39ed7ad3897ec2816
ikki-sasa <manga.samadjine@3wa.io>
2022-02-17 20:33:11 +0100

    modification categoryType

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Category;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +14,51 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('slug')
-            ->add('description')
-            ->add('img')
-            ->add('parentCategory')
+            ->add('name', TextType::class, [
+                'required' => true,
+                'label' => 'Nom Produit',
+                'attr' => [
+                    'maxLenght' => 255,
+                    'placeholder' => 'Exemple: L\'Amoureux Noir'
+                ]
+            ])
+
+            // ->add('slug', TextType::class, [
+            //     'required' => true,
+            //     'label' => '',
+            //     'attr' => [
+            //     'maxLenght' => 255,
+
+            //     ]
+            // ])
+
+            ->add('description', TextareaType::class, [
+                'required' => true,
+                'label' => 'Description produit',
+                'attr' => [
+                    'maxLength' => 80000,
+                    'placeholder' => 'Exemple: Voici le nouveau venu dans la famille des ganaches.... '
+                ]
+            ])
+
+            ->add('img', FileType::class, [
+                'required' => true,
+                'label' => 'Image produit',
+                'mapped' => false,
+                'help' => 'jpeg, png, gif, svg, eps, psd, tiff, jp2 ou webp - 3 Mo maximum ',
+                'constraints' => [
+                    'maxSize' => '3M',
+                    'maxSizeMessage' => 'Votre fichier est trop volumineux'
+                ]
+            ])
+
+            ->add('parentCategory', ::class, [
+                'required' => true,
+                'label' => '',
+                'attr' => [
+                    ''
+                ]
+            ])
         ;
     }
 
